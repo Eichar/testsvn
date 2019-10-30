90b4f94bafdf4b3813c33dcc868717d57ade2255
hurong <hurong@ff6ee3ff-e251-8d43-a86f-1d75d59d4c44>
2019-10-30 11:43:40 +0000

    第一次提交
    git-svn-id: https://127.0.0.1:8443/svn/TestJenkins@16 ff6ee3ff-e251-8d43-a86f-1d75d59d4c44

---
+++ src/main/java/com/hurong/testjenkins1/Controller.java
@@ -0,0 +1,17 @@
+package com.hurong.testjenkins1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+
+    @RequestMapping(path = "/ping")
+    public String ping() {
+
+        return "这是第 2 次gittest!";
+
+
+    }
+}
---
+++ src/main/java/com/hurong/testjenkins1/Testjenkins1Application.java
@@ -0,0 +1,13 @@
+package com.hurong.testjenkins1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Testjenkins1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Testjenkins1Application.class, args);
+	}
+
+}
